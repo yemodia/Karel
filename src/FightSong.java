67da42c5b96032516dbc68c99ad15e41ccb2ba2f
diaye <yerim-oumar.dia@ssfs.org>
2019-10-03 10:13:06 -0400

    Finished writing the methods to print lyrics to a song.

---
+++ src/FightSong.java
@@ -0,0 +1,32 @@
+public class FightSong {
+    public static void lyricOne() {
+        System.out.println("Go, team, go!");
+        System.out.println("You can do it.");
+    }
+
+    public static void lyricTwo() {
+        System.out.println("You're the best, ");
+        System.out.println("In the West.");
+    }
+
+    public static void blankLine() {
+        System.out.println("     ");
+    }
+    public static void main(String[] args){
+    lyricOne();
+    blankLine();
+
+    lyricOne();
+    lyricTwo();
+    lyricOne();
+    blankLine();
+
+    lyricOne();
+    lyricTwo();
+    lyricOne();
+    blankLine();
+
+    lyricOne();
+    }
+}
+
